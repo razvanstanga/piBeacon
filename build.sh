ebbc5d0bf21606518aad29be86f11af1b147d51c
Razvan Stanga <git@razvi.ro>
2015-07-24 13:03:09 +0000

    allow multuple bluetooth adaptors

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb 
 sudo dpkg -i node_latest_armhf.deb
 npm install eddystone-beacon
+npm install forever-monitor
+npm install forever -g
+ln -s /usr/local/bin/node /usr/bin/node
 
 apt-get install bluetooth bluez-utils blueman libbluetooth-dev
 
-ln -s init/piBeacon /etc/init.d/piBeacon
-update-rc.d piBeacon defaults
+ln -s init/piBeacon /etc/init.d/piBeacon.0
+update-rc.d piBeacon.0 defaults
