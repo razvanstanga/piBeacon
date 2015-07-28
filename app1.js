c90ca250fac5aa7be7ab3976339f18d2b9d5fc4e
Razvan Stanga <git@razvi.ro>
2015-07-28 09:21:56 +0000

    apps code in config

--- app1.js
+++ app1.js
@@ -1,21 +1,2 @@
-var config = require('./init');
-var request = require('request');
-var beacon = require('eddystone-beacon');
-
-setInterval(function(){
-	if (config.SERVER){
-		request({
-			url: config.SERVER+'&macaddress'+config.macaddress,
-			json: true
-		}, function (error, response, body) {
-			if (!error && response.statusCode === 200) {
-				if (body.url != config.urlcache) {
-					beacon.advertiseUrl(body.url);
-					config.urlcache = body.url;
-				}
-			}
-		});
-	} else {
-		beacon.advertiseUrl("http://www.example2.com/");
-	}
-}, config.INTERVAL);
+var config = require('./config');
+config.daemon(1);
--- app3.js
+++ app3.js
@@ -1,21 +1,3 @@
-var config = require('./init');
-var request = require('request');
-var beacon = require('eddystone-beacon');
+var config = require('./config');
+config.daemon(0);
 
-setInterval(function(){
-	if (config.SERVER){
-		request({
-			url: config.SERVER+'&macaddress'+config.macaddress,
-			json: true
-		}, function (error, response, body) {
-			if (!error && response.statusCode === 200) {
-				if (body.url != config.urlcache) {
-					beacon.advertiseUrl(body.url);
-					config.urlcache = body.url;
-				}
-			}
-		});
-	} else {
-		beacon.advertiseUrl("http://www.example4.com/");
-	}
-}, config.INTERVAL);
